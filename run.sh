8a3aefffb321a01bd2cff51891e30034b08cb803
Guilherme <guilpejon@gmail.com>
2022-02-05 17:05:33 -0300

    Added deployments

--- run.sh
+++ run.sh
@@ -1,19 +1,49 @@
-# liga o pod definido em k8s/pod.yaml
-kubectl apply -f k8s/pod.yaml
+Deployment -> ReplicaSet -> Pod
+
+#########
+# LISTING
+#########
+
+# lista replicasets
+kubectl get replicasets
+kubectl get replicaset
 
 # lista todos os pods
 kubectl get pods
 kubectl get pod
 kubectl get po
 
-# adiciona um mapping da porta 8000 do host para a porta 80 do pod de nome pod/goserver
-kubectl port-forward pod/goserver 8000:80
+# list todos os deployments
+kubectl get deployments
+kubectl get deployment
 
-# deleta o pod de nome `goserver`
-kubectl delete pod goserver
+##########
+# STARTING
+##########
+
+# liga o pod definido em k8s/pod.yaml
+kubectl apply -f k8s/pod.yaml
 
 # cria o replicaset
 kubectl apply -f k8s/replicaset.yaml
 
-# lista replicasets
-kubectl get replicasets
+# cria o deployment
+kubectl apply -f k8s/deployment.yaml
+
+##########
+# DELETING
+##########
+
+kubectl delete pod <NAME>
+
+kubectl delete replicaset <NAME>
+
+########
+# RANDOM
+########
+
+# adiciona um mapping da porta 8000 do host para a porta 80 do pod de nome pod/goserver
+kubectl port-forward pod/goserver 8000:80
+
+# imprime as infos do pod
+kubectl describe pod <NAME>
